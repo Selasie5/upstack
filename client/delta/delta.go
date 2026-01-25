13a5f40aa11956dd6d529bde6008b3292113cc46
Selasie5 <selasisepenu5@gmail.com>
2026-01-25 20:55:00 +0000

    feat:define chunk size

--- client/delta/delta.go
+++ client/delta/delta.go
@@ -1,3 +1,9 @@
-// Responsible for delta management in the client application
-
 package delta
+
+const chunkSize = 4 * 1024 * 1024 // 4MB chunk size for delta transfers
+
+type Chunk struct {
+	Index int
+	Data  []byte
+	Hash  string
+}
